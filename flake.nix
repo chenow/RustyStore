3161b4c561b3fddde79f721f9869c11bd56cacd9
chenow <antoine.cheneau@outlook.com>
2024-09-18 14:33:29 +0200

    feat(nix): support installation of crates

--- flake.nix
+++ flake.nix
@@ -2,8 +2,12 @@
   description = "A Nix flake for a Redis Clone in Rust";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     pre-commit-env = {
       url = "github:chenow/nix-pre-commit";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,24 +16,32 @@
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      rust-overlay,
       pre-commit-env,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [
+          (import rust-overlay)
+          (self: super: {
+            rust-toolchain = self.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
         pre-commit-lib = pre-commit-env.lib.${system};
       in
       {
         devShells.default = pre-commit-lib.mkDevShell {
           extraPackages = with pkgs; [
-            rustc
-            cargo
-            rustfmt
-            clippy
+            rust-toolchain
+            cargo-watch
+            cargo-nextest
             redis
           ];
         };
